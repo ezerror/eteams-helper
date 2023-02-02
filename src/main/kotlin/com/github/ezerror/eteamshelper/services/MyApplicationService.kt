728d5cd92fad3e4f2f1c3bef44826f974c573500
GitHub Action <action@github.com>
2023-02-02 14:11:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/ezerror/eteamshelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ezerror/eteamshelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ezerror.eteamshelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ezerror.eteamshelper.MyBundle
 
 class MyApplicationService {
 
